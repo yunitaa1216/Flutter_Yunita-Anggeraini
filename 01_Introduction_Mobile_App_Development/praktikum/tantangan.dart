b1f3660b1ff8993c3a67c72f493b5f2d98770321
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-08-18 17:17:27 +0800

    Create tantangan.dart

---
+++ 01_Introduction_Mobile_App_Development/praktikum/tantangan.dart
@@ -0,0 +1,15 @@
+//buat variable yang berfungsi untuk menghitung keliling peserti panjang dan segitiga
+int panjang_persegip = 8;
+int lebar_persegip = 4;
+int keliling_persegip = 2 * (panjang_persegip + lebar_persegip);
+
+int panjangsisiA = 5;
+int PanjangsisiB = 6;
+int PanjangsisiC = 7;
+
+int keliling_segitiga = panjangsisiA + PanjangsisiB + PanjangsisiC;
+
+void main(){
+   print('hasil keliling persegi panjang $keliling_persegip');
+   print('hasil keliling segitiga $keliling_segitiga');
+ }
