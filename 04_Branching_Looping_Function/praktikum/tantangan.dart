23c7f5c5c4da20e467a9aa786ff9e6d8df779f9d
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-08-21 16:38:20 +0800

    Update tantangan.dart

--- 04_Branching_Looping_Function/praktikum/tantangan.dart
+++ 04_Branching_Looping_Function/praktikum/tantangan.dart
@@ -5,11 +5,20 @@ double tas = 100000;
 double total_pembelian = 200000;
 double diskon = 0.1;
 
+//no 1
 double jumlah_pembelian(double buku, double pensil, double tas){
   return buku + pensil + tas;
+}
+
+////no 2
+double totalDiskon(double total_pembelian, double diskon){
+  return total_pembelian * diskon;
 }
 
 void main(){
   double jumlahbeli = jumlah_pembelian(buku, pensil, tas);
   print('Jumlah pembelian $jumlahbeli');
+
+  double JumlahDiskon = totalDiskon(total_pembelian, diskon);
+  print("Jumlah Diskon dari total pembelian: $JumlahDiskon");
 }
