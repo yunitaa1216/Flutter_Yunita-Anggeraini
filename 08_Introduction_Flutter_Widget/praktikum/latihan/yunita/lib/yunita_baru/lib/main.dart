0f366ccc7719eb2a0182c7ddbcd51d89cbb3f0c1
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-09-15 16:01:06 +0800

    Update main.dart

--- 08_Introduction_Flutter_Widget/praktikum/latihan/yunita/lib/yunita_baru/lib/main.dart
+++ 08_Introduction_Flutter_Widget/praktikum/latihan/yunita/lib/yunita_baru/lib/main.dart
@@ -1,3 +1,49 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text('Halo Alterra'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'Nama saya adalah Yunita Anggeraini',
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
