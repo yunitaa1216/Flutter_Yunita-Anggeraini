d9db65f6bf795dbfe6839825280c4c08144484e6
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-09-16 06:47:36 +0800

    Update main.dart

--- 08_Introduction_Flutter_Widget/praktikum/latihan/yunita/lib/yunita_baru/lib/main.dart
+++ 08_Introduction_Flutter_Widget/praktikum/latihan/yunita/lib/yunita_baru/lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,9 +19,29 @@ class MyApp extends StatelessWidget {
       home: const MyHomePage(),
     );
   }
+}
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Halo Alterra'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Nama saya adalah Yunita Anggeraini',
+            ),
+          ]),
+      ),
+    );
+  }
 }
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
