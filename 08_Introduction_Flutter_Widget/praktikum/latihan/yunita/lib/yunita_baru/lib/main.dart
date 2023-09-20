d401f60dc29c5c693b790c2713acb85bb2df6edc
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-09-20 10:23:27 +0800

    Update main.dart

--- 08_Introduction_Flutter_Widget/praktikum/latihan/yunita/lib/yunita_baru/lib/main.dart
+++ 08_Introduction_Flutter_Widget/praktikum/latihan/yunita/lib/yunita_baru/lib/main.dart
@@ -1,48 +1,48 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Halo Alterra'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Nama saya adalah Yunita Anggeraini',
-            ),
-          ]),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text('Halo Alterra'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'Nama saya adalah Yunita Anggeraini',
+//             ),
+//           ]),
+//       ),
+//     );
+//   }
+// }
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
 // }
@@ -90,196 +90,258 @@ class MyHomePage extends StatelessWidget {
 //     );
 //   }
 // }
-// import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+import 'package:flutter/material.dart';
 
-// class MyApp extends StatefulWidget {
-//   const MyApp({Key? key}) : super(key: key);
+void main() {
+  runApp(const MyApp());
+}
 
-//   @override
-//   State<MyApp> createState() => _MyAppState();
-// }
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-// class _MyAppState extends State<MyApp> {
-//   var inputController = TextEditingController();
-//   var radioValue = '';
-//   bool checkValue = false;
-//   var dropDownValue = 0;
-//   List data_kontak = [
-//     {"title": "Kepala SUku", "jenis_kelamin": "Laki-laki"},
-//     {"title": "Guru Bahasa Cinta", "jenis_kelamin": "perempuan"},
-//     {"title": "Bendahara Sekolah", "jenis_kelamin": "Laki-laki"},
-//     {"title": "Guru Matematika", "jenis_kelamin": "Perempuan"},
-//     {"title": "Guru Biologi", "jenis_kelamin": "Laki-laki"}
-//   ]; 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Yunita App'),
-//         ),
-//         body: SingleChildScrollView( // Wrap your content in a SingleChildScrollView
-//           child: Container(
-//             color: Color.fromARGB(255, 150, 74, 74),
-//             margin: EdgeInsets.only(top: 30, left: 20, right: 20),
-//             child: Center(
-//               child: Column(
-//                 children: [
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                     children: [
-//                       Text('Yunita'),
-//                       Text('Yunita')
-//                     ],
-//                   ),
-//                   SizedBox(
-//                     height: 60,
-//                   ),
-//                   Text('List Peserta Flutter Kelas D'),
-//                   TextField(
-//                     controller: inputController,
-//                     onChanged: (inputController) {
-//                       print('$inputController');
-//                     },
-//                   ),
-//                   Row(
-//                     children: [
-//                       Radio(
-//                           value: 'Laki-laki',
-//                           groupValue: radioValue,
-//                           onChanged: (String? value) {
-//                             setState(() {
-//                               radioValue = value ?? '';
-//                               print('Nilai  dari radio value =  $radioValue');
-//                             });
-//                           }),
-//                       Text('Laki - laki'),
-//                     ],
-//                   ),
+class _MyAppState extends State<MyApp> {
+  // var inputController = TextEditingController();
+  var inputController = TextEditingController(text: '');
+  var radioValue = '';
+  bool checkValue = false;
+  var dropDownValue = 0;
+  List data_kontak = [
+    {
+      "title": "Mahasiswa", 
+      "jenis_kelamin": "Laki-laki"
+    },
+    {
+      "title": "Mahasiswa", 
+      "jenis_kelamin": "perempuan"
+    },
+    {
+      "title": "Mahasiswa", 
+      "jenis_kelamin": "Laki-laki"
+    },
+    {
+      "title": "Mahasiswa", 
+      "jenis_kelamin": "Perempuan"
+    },
+  ]; 
 
-//                   Row(
-//                     children: [
-//                       Radio(
-//                           value: 'Perempuan',
-//                           groupValue: radioValue,
-//                           onChanged: (String? value) {
-//                             setState(() {
-//                               radioValue = value ?? '';
-//                               print('Nilai  dari radio value =  $radioValue');
-//                             });
-//                           }),
-//                       Text('Perempuan'),
-//                     ],
-//                   ),
-//                   //checkbox simple
-//                   Divider(),
-//                   Row(
-//                     children: [
-//                       Checkbox(
-//                           value: checkValue,
-//                           checkColor: Colors.green,
-//                           onChanged: (bool? value) {
-//                             setState(() {
-//                               checkValue = value!;
-//                               print('$checkValue');
-//                             });
-//                           }),
-//                       Text('C++'),
-//                     ],
-//                   ),
-//                   Row(
-//                     children: [
-//                       Checkbox(
-//                           value: checkValue,
-//                           checkColor: Colors.green,
-//                           onChanged: (bool? value) {
-//                             setState(() {
-//                               checkValue = value!;
-//                               print('$checkValue');
-//                             });
-//                           }),
-//                       Text('Dart'),
-//                     ],
-//                   ),
-//                   Row(
-//                     children: [
-//                       Checkbox(
-//                           value: checkValue,
-//                           checkColor: Colors.green,
-//                           onChanged: (bool? value) {
-//                             setState(() {
-//                               checkValue = value!;
-//                               print('$checkValue');
-//                             });
-//                           }),
-//                       Text('PHP'),
-//                     ],
-//                   ),
-//                   Divider(),
-//                   DropdownButton(
-//                       value: dropDownValue,
-//                       items: [
-//                         DropdownMenuItem(
-//                           value: 0,
-//                           child: Text('pilihan 1'),
-//                         ),
-//                         DropdownMenuItem(value: 1, child: Text('pilihan 2')),
-//                         DropdownMenuItem(value: 2, child: Text('pilihan 3'))
-//                       ],
-//                       onChanged: (int? value) {
-//                         setState(() {
-//                           dropDownValue = value ?? 0;
-//                           print('$dropDownValue');
-//                         });
-//                       }),
-//                   ElevatedButton(
-//                     child: Text('Submit'),
-//                     onPressed: () {
-//                       setState(() {
-//                         data_kontak.add({
-//                           "title": "${inputController.text}",
-//                           "jenis_kelamin": "$radioValue",
-//                           "checkbox": checkValue,
-//                           "dropdown": dropDownValue,
-//                         });
-//                       });
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Yunita App'),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            color: Color.fromARGB(255, 149, 149, 189),
+            margin: EdgeInsets.only(top: 30, left: 20, right: 20),
+            child: Center(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Yunita'),
+                      Text('Yunita')
+                    ],
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Text('List Peserta Flutter Kelas D'),
+                  TextField(
+                    controller: inputController,
+                    onChanged: (inputController) {
+                      print('$inputController');
+                    },
+                  ),
+                  Row(
+                    children: [
+                      Radio(
+                          value: 'Laki-laki',
+                          groupValue: radioValue,
+                          onChanged: (String? value) {
+                            setState(() {
+                              radioValue = value ?? '';
+                              print('Nilai  dari radio value =  $radioValue');
+                            });
+                          }),
+                      Text('Laki - laki'),
+                    ],
+                  ),
 
-//                       print('data_kontak : $data_kontak');
-//                     },
-//                   ),
-//                   SizedBox(
-//                       height: 200,
-//                       child: ListView.builder(
-//                           itemCount: data_kontak.length,
-//                           itemBuilder: (BuildContext context, int index) {
-//                             return ListTile(
-//                               leading: FlutterLogo(),
-//                               title: Text('${data_kontak[index]["title"]}'),
-//                               subtitle:
-//                                   Text('${data_kontak[index]["jenis_kelamin"]}'),
-//                             trailing: Column(
-//                             children: [
-//                               Text('Checkbox: ${data_kontak[index]["checkbox"]}'),
-//                               Text('Dropdown: ${data_kontak[index]["dropdown"]}'),
-//                             ],
-//                               ),
-//                             );
-//                           })
-//                   ),
-//                   SizedBox(
-//                     height: 20,
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+                  Row(
+                    children: [
+                      Radio(
+                          value: 'Perempuan',
+                          groupValue: radioValue,
+                          onChanged: (String? value) {
+                            setState(() {
+                              radioValue = value ?? '';
+                              print('Nilai  dari radio value =  $radioValue');
+                            });
+                          }),
+                      Text('Perempuan'),
+                    ],
+                  ),
+                  //checkbox simple
+                  // Divider(),
+                  // Row(
+                  //   children: [
+                  //     Checkbox(
+                  //         value: checkValue,
+                  //         checkColor: Colors.green,
+                  //         onChanged: (bool? value) {
+                  //           setState(() {
+                  //             checkValue = value!;
+                  //             print('$checkValue');
+                  //           });
+                  //         }),
+                  //     Text('C++'),
+                  //   ],
+                  // ),
+                  // Row(
+                  //   children: [
+                  //     Checkbox(
+                  //         value: checkValue,
+                  //         checkColor: Colors.green,
+                  //         onChanged: (bool? value) {
+                  //           setState(() {
+                  //             checkValue = value!;
+                  //             print('$checkValue');
+                  //           });
+                  //         }),
+                  //     Text('Dart'),
+                  //   ],
+                  // ),
+                  // Row(
+                  //   children: [
+                  //     Checkbox(
+                  //         value: checkValue,
+                  //         checkColor: Colors.green,
+                  //         onChanged: (bool? value) {
+                  //           setState(() {
+                  //             checkValue = value!;
+                  //             print('$checkValue');
+                  //           });
+                  //         }),
+                  //     Text('PHP'),
+                  //   ],
+                  // ),
+                  // Divider(),
+                  // DropdownButton(
+                  //     value: dropDownValue,
+                  //     items: [
+                  //       DropdownMenuItem(
+                  //         value: 0,
+                  //         child: Text('pilihan 1'),
+                  //       ),
+                  //       DropdownMenuItem(value: 1, child: Text('pilihan 2')),
+                  //       DropdownMenuItem(value: 2, child: Text('pilihan 3'))
+                  //     ],
+                  //     onChanged: (int? value) {
+                  //       setState(() {
+                  //         dropDownValue = value ?? 0;
+                  //         print('$dropDownValue');
+                  //       });
+                  //     }),
+                  ElevatedButton(
+                    child: Text('Submit'),
+                    onPressed: () {
+                      setState(() {
+                        data_kontak.add({
+                          "title": "${inputController.text}",
+                          "jenis_kelamin": "$radioValue",
+                          // "checkbox": checkValue,
+                          // "dropdown": dropDownValue,
+                        });
+                      });
+
+                      print('data_kontak : $data_kontak');
+                    },
+                  ),
+                  SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                          itemCount: data_kontak.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return ListTile(
+                              leading: FlutterLogo(),
+                              title: Text('${data_kontak[index]["title"]}'),
+                              subtitle:
+                                  Text('${data_kontak[index]["jenis_kelamin"]}'),
+                            trailing: Wrap(
+                            children: [
+                              IconButton(onPressed: (){
+                                // print('delete $data_kontak');
+                                setState(() {
+                                  data_kontak.removeAt(index);
+                                });
+                              }, 
+                              icon: Icon(Icons.delete)),
+                              IconButton(
+  onPressed: () {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        TextEditingController textEditingController = TextEditingController(text: data_kontak[index]["title"]);
+
+        return AlertDialog(
+          title: Text('Konfirmasi Edit'),
+          content: Column(
+            children: [
+              Text('nama'),
+              TextField(
+                controller: textEditingController,
+                decoration: InputDecoration(
+                  hintText: 'Masukkan nama',
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, 'Batal');
+              },
+              child: Text('Batal'),
+            ),
+            TextButton(
+              onPressed: () {
+                final editText = textEditingController.text;
+                setState(() {
+                  data_kontak[index]["title"] = editText;
+                });
+                Navigator.pop(context);
+              },
+              child: Text('Edit'),
+            ),
+          ],
+        );
+      },
+    );
+  },
+  icon: Icon(Icons.edit),
+),
+                            ],
+                            ),
+                            );
+                          })
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
