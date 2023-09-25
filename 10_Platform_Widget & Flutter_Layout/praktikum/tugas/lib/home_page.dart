61efe4e93d01ef8b485b0858f42f4772bf163e6b
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-09-25 13:09:37 +0800

    Tugas SI Alterra Academy

---
+++ 10_Platform_Widget & Flutter_Layout/praktikum/tugas/lib/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Assets'),
+        centerTitle: true,
+      ),
+      body: ListView(children: [
+        Image(
+          height: 350,
+  image: AssetImage('assets/bg.jpg'),
+),
+// Text('Tes Font', style: TextStyle(fontSize: 30, fontFamily: 'Rowdies'),),
+Text(
+  'Tes Font', 
+  style: GoogleFonts.poppins(fontSize: 30),
+  ),
+// Image(
+//   height: 150,
+//   image: AssetImage('assets/logo.png'),
+// ),
+// Image.asset('assets/logo.png'),
+// Image.network('https://picsum.photos/id/237/200/300'),
+      ],),
+    );
+  }
+}
---
+++ 13_Assets/README.md
@@ -0,0 +1,11 @@
+<h1><center>Assets</center></h1>
+
+<p>Assets adalah file yang di bundled dan di deployed bersama aplikasi. Tipe-tipe assets misalnya icons, images, JSON files dll.</p>
+
+### Image
+Flutter mendukung format gambar seperti JPEG, WebP, GIF, PNG, BMP dll. Untuk menampilkan gambar pada aplikasi dapat diambil dari assets atau internet.
+- Method Image.assset digunakan untuk mendapatkan gambar yang sudah ditambahkan dalam project.
+- Method Image.network digunakan untuk mendapatkan gambar melalui internet menggunakan string urlnya.
+
+### Font
+<p>Penerapan font dapat menggunakan custom font atau dari package</p>
