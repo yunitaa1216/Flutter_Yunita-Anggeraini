de7cfdeed59c2f46f899301b720ef9b86f0ef664
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-09-30 15:51:43 +0800

    Tugas SI Alterra Academy

--- 13_Assets/praktikum/assets_bottomsheet_navigation/lib/main.dart
+++ 13_Assets/praktikum/assets_bottomsheet_navigation/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'contact.dart';
+import 'galeri.dart';
 import 'home_page.dart';
 // import 'ui/screen/galeri.dart';
 void main() {
@@ -7,168 +8,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
       title: 'Flutter Assets',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        // fontFamily: 'Rowdies',
       ),
-      home: const HomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/galeri': (context) => GaleriPage(),
+        '/contact': (context) => ContactPage(),
+      },
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   ListTile buildListTile(String name) {
-//     return ListTile(
-//       title: Text(name),
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//         backgroundColor: Color(0xFFDCD0FF),
-//           title: Center(
-//           child: Text('Contacts',
-//           style: TextStyle(
-//             color: Colors.black
-//           ),),
-//           ),
-//         actions: [
-//           IconButton(
-//             icon: Icon(Icons.search),
-//             onPressed: () {},
-//           ),
-//         ],
-//         ),
-//         body: Center(
-//         child: Column(
-//           children: [
-//             SizedBox(height: 16.0),
-//             Icon(
-//               Icons.contact_page_outlined,
-//               size: 32.0,
-//               color: Color.fromARGB(255, 200, 186, 249),
-//             ),
-//             SizedBox(height: 16.0),
-//             Text(
-//               'Create New Contacts',
-//               style: TextStyle(
-//                 fontSize: 24.0,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             Text('To keep your contacts organized and easily accessible, please provide the following information to create a new contact. You can enter the person\'s name and phone number, and we\'ll take care of the rest. Creating a new contact has never been easier!',
-//             style: TextStyle(
-//                 color: Color(0xFF5F5E5E),
-//               ),
-//             ),
-//             SizedBox(height: 16.0),
-//               Column(
-//                 children: [
-//                   Container(
-//                     padding: EdgeInsets.all(16.0),
-//                     color: Color(0xFFDCD0FF),
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Text(
-//                           'Name',
-//                           style: TextStyle(
-//                             fontWeight: FontWeight.bold,
-//                           ),
-//                         ),
-//                         SizedBox(height: 4.0), // Jarak antara label "Name" dan input
-//                         TextField(
-//                           decoration: InputDecoration(
-//                             hintText: 'Insert your name',
-//                             border: InputBorder.none,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                   SizedBox(height: 16.0),
-//                   Container(
-//                     padding: EdgeInsets.all(16.0),
-//                     color: Color(0xFFDCD0FF),
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         Text(
-//                           'Nomor',
-//                           style: TextStyle(
-//                             fontWeight: FontWeight.bold,
-//                           ),
-//                         ),
-//                         SizedBox(height: 2.0),
-//                         TextField(
-//                           decoration: InputDecoration(
-//                             hintText: '+62 .....',
-//                             border: InputBorder.none,
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                   SizedBox(height: 6.0),
-//                   Align(
-//                     alignment: Alignment.centerRight,
-//                     child: ElevatedButton(
-//                       onPressed: () {},
-//                       style: ElevatedButton.styleFrom(
-//                         backgroundColor: Color.fromARGB(255, 91, 76, 138),
-//                         shape: RoundedRectangleBorder(
-//                           borderRadius: BorderRadius.circular(50.0),
-//                         ),
-//                       ),
-//                       child: Text(
-//                         'Submit',
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//           ],
-//         ),
-//         ),
-//         bottomNavigationBar: BottomNavigationBar(
-//           backgroundColor: Color(0xFFDCD0FF),
-//           items: const <BottomNavigationBarItem>[
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.favorite),
-//               label: 'Favorites',
-//             ),
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.search),
-//               label: 'Search',
-//             ),
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.info),
-//               label: 'Information',
-//             ),
-//           ],
-//           selectedItemColor: Colors.black,
-//           unselectedItemColor: Colors.black,
-//         ),
-//       ),
-//     );
-//   }
-// }
