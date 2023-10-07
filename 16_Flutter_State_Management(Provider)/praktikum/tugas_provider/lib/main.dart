e350b00e824ae1aa05f3f0c6d311bb87332cbf9e
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-10-07 19:12:46 +0800

    Tugas SI Alterra Academy

--- 16_Flutter_State_Management(Provider)/praktikum/tugas_provider/lib/main.dart
+++ 16_Flutter_State_Management(Provider)/praktikum/tugas_provider/lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; // Import the Provider package
 import 'contact.dart';
 import 'galeri.dart';
 import 'home_page.dart';
+
 // import 'ui/screen/galeri.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AppState(), // Create an instance of your AppState
+      child: const MyApp(),
+    ),
+  );
+}
+class AppState extends ChangeNotifier {
+  bool someFlag = false; // Replace with your application state variables
+
+  void updateFlag(bool newValue) {
+    someFlag = newValue;
+    notifyListeners(); // Notify listeners of the change
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +27,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final appState = Provider.of<AppState>(context); // Access the app state
+    appState.updateFlag(true); // Update the state
+    bool flagValue = appState.someFlag; // Access the state
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Assets',
--- 19_Introduction_REST_API_JSON/praktikum/latihan/lib/main.dart
+++ 19_Introduction_REST_API_JSON/praktikum/latihan/lib/main.dart
@@ -1,17 +1,49 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:latihan/stores/contact.dart' as contact_store;
+// import 'package:dio/dio.dart';
+
+// import 'home_page.dart';
+// // import 'state_management.dart';
+// void main() {
+//   runApp(MultiProvider(providers: [
+//     ChangeNotifierProvider(create: (_) => contact_store.Contact(),
+//     ),
+//   ],
+//   child: const MyApp(),
+//   ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: 'Phone Book App',
+//       home: HomePage(),
+//       // title: 'Flutter State Managemt',
+//       // home: StateManagement(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:latihan/stores/contact.dart' as contact_store;
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart'; // Import Dio
 
-import 'home_page.dart';
-// import 'state_management.dart';
 void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (_) => contact_store.Contact(),
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => contact_store.Contact(),
+        ),
+      ],
+      child: const MyApp(),
     ),
-  ],
-  child: const MyApp(),
-  ),
   );
 }
 
@@ -20,11 +52,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final dio = Dio(); // Buat instance Dio di sini
+
+    return MaterialApp(
       title: 'Phone Book App',
-      home: HomePage(),
-      // title: 'Flutter State Managemt',
-      // home: StateManagement(),
+      home: ChangeNotifierProvider.value(
+        value: dio, // Berikan instance Dio ke HomePage atau Contact
+        child: HomePage(),
+      ),
     );
   }
 }
