92917a60868ebd02741d1ba21fe1b0d0efd570b5
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-10-11 15:16:11 +0800

    Tugas SI Alterra Academy

--- Weekly2/project/tugas_yunita/lib/home_page.dart
+++ Weekly2/project/tugas_yunita/lib/home_page.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+List<Map<String, dynamic>> daftarJasaKos = [
+  {
+    'gambar': 'assets/kos_1.jpg',
+    'nama': 'Kos Amanah',
+    'deskripsi': 'Kos nyaman dengan fasilitas lengkap di pusat kota.',
+    'harga': 'Rp 1.000.000/bulan',
+  },
+  {
+    'gambar': 'assets/kos_2.jpg',
+    'nama': 'Kos Harmoni',
+    'deskripsi': 'Kos bersih dengan akses mudah ke transportasi umum.',
+    'harga': 'Rp 800.000/bulan',
+  },
+  {
+    'gambar': 'assets/kos_3.jpg',
+    'nama': 'Kos Bahagia',
+    'deskripsi': 'Kos modern dengan wifi cepat dan area bersama.',
+    'harga': 'Rp 1.200.000/bulan',
+  },
+  {
+    'gambar': 'assets/kos_4.jpg',
+    'nama': 'Kos Sejahtera',
+    'deskripsi': 'Kos strategis dekat dengan pusat perbelanjaan.',
+    'harga': 'Rp 900.000/bulan',
+  },
+];
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key});
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -65,97 +92,188 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-              // Bagian "About Us"
               Padding(
                 padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'About Us',
+                        style: GoogleFonts.openSans(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Daftar Kos',
+                        style: GoogleFonts.openSans(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: (daftarJasaKos.length / 2).ceil(),
+                        itemBuilder: (context, index) {
+                          final firstJasaKos = daftarJasaKos[index * 2];
+                          final secondJasaKos = (index * 2 + 1 < daftarJasaKos.length) ? daftarJasaKos[index * 2 + 1] : null;
+                          return Row(
+                            children: [
+                              Expanded(
+                                child: Card(
+                                  child: Column(
+                                    children: [
+                                      AspectRatio(
+                                        aspectRatio: 16 / 9,
+                                        child: Image.asset(
+                                          firstJasaKos['gambar'],
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      ListTile(
+                                        title: Text(firstJasaKos['nama']),
+                                        subtitle: Text(firstJasaKos['deskripsi']),
+                                      ),
+                                      ListTile(
+                                        title: Text(firstJasaKos['harga']),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              if (secondJasaKos != null)
+                                Expanded(
+                                  child: Card(
+                                    child: Column(
+                                      children: [
+                                        AspectRatio(
+                                          aspectRatio: 16 / 9,
+                                          child: Image.asset(
+                                            secondJasaKos['gambar'],
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                        ListTile(
+                                          title: Text(secondJasaKos['nama']),
+                                          subtitle: Text(secondJasaKos['deskripsi']),
+                                        ),
+                                        ListTile(
+                                          title: Text(secondJasaKos['harga']),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                              ),
+                            ],
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Column(
                   children: [
                     Text(
-                      'About Us',
+                      'Contact Us',
                       style: GoogleFonts.openSans(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 10),
-                    // Daftar card jasa kos yang bisa di-booking
-                    ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: 3, // Ganti dengan jumlah jasa kos yang sesuai
-                      itemBuilder: (context, index) {
-                        return Card(
-                          // Konfigurasi card sesuai dengan data jasa kos
-                          child: ListTile(
-                            title: Text('Nama Jasa Kos $index'),
-                            subtitle: Text('Deskripsi Jasa Kos $index'),
-                            // Tambahkan tombol booking atau aksi lainnya di sini
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Nama',
+                              border: OutlineInputBorder(),
+                            ),
                           ),
-                        );
-                      },
+                          SizedBox(height: 10),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          ElevatedButton.icon(
+                            onPressed: () {
+                            },
+                            icon: Icon(Icons.send),
+                            label: Text('Send'),
+                            style: ElevatedButton.styleFrom(
+                              primary: Color.fromARGB(255, 22, 41, 94),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Color.fromARGB(255, 255, 255, 255),
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_filled),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Search',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.black,
-        ),
-        endDrawer: Drawer(
-          child: Container(
-            color: Colors.black,
-            child: ListView(
-              children: <Widget>[
-                ListTile(
-                  title: Text(
-                    'Contact Us',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  title: Text(
-                    'About Us',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  title: Text(
-                    'Login',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
                 ),
               ],
             ),
           ),
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Color.fromARGB(255, 255, 255, 255),
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_filled),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+            ],
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.black,
+          ),
+          endDrawer: Drawer(
+            child: Container(
+              color: Colors.black,
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    title: Text(
+                      'Contact Us',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: Text(
+                      'About Us',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),
-    );
-  }
+      );
+    }
 }
